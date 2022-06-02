13ced81dab43a59e7a1dbf65ac247e42b558ce26
Roman Rodin <romaro83@gmail.com>
2022-06-02 06:31:28 +0300

    BasePage: add placeholders getter

--- packages/backend/core/BasePage.ts
+++ packages/backend/core/BasePage.ts
@@ -1,4 +1,4 @@
-import { GlobalContext } from './types/context';
+import { ActionContext, GlobalContext } from './types/context';
 
 export interface PageInstance extends BasePage {
     httpStatus: HttpStatus;
@@ -8,9 +8,25 @@ export class BasePage {
     protected templateDir: string;
     private templater;
 
-    constructor(private g: GlobalContext) {
+    /**
+     * Плейсхолдеры уровня страницы, которые доступны в шаблонизаторе
+     * по ссылкам вида props.placeholderName. Могут быть переданы в props
+     * при инициализации страницы, либо через вызов метода setPlaceholder()
+     */
+    private propsPlaceholders: Placeholders;
+
+    /**
+     * В отличие от propsPlaceholders, эти плейсхолдеры явно прописываются
+     * в классе страницы. Это максимально статичные данные.
+     */
+    private staticPlaceholders: Placeholders;
+
+    constructor(
+        private g: GlobalContext, private ctx: ActionContext, private props: Placeholders = {}) {
         this.templateDir = this.g.config.templateDir;
         this.templater = this.g.templater;
+        this.propsPlaceholders = this.props;
+        this.staticPlaceholders = {};
     }
 
     public async render(): Promise<string> {
@@ -22,9 +38,32 @@ export class BasePage {
         }
     }
 
+    public setPlaceholder(name: string, value: PlaceholderValue): void {
+        this.propsPlaceholders[name] = value;
+    }
+
+    private checkRootName(name: string): void {
+        const reservedNames = ['user', 'static', 'page', 'props'];
+
+        if (reservedNames.includes(name)) {
+            throw new Error(`Root name [ ${name} ] is reserved`);
+        }
+    }
+
+    /**
+     * Объект шаблонизации
+     * На первом уровне вложенности должны находиться только объекты.
+     * Причем каждый из таких объектов группирует плейсхолдеры по источнику данных.
+     * Например, в dict.sendButtonName сразу понятно, что плейсхолдер
+     * sendButtonName был инициализирован в словаре самой странице.
+     * А в случае с BlockName.dict.sendButtonName уже в словаре
+     * подключенного к странице компонента BlockName.
+     */
     private get placeholders(): Placeholders {
-        // Todo
-        return {};
+        return {
+            static: this.staticPlaceholders,
+            props: this.propsPlaceholders
+        };
     }
 
     private get pageName(): string {
--- packages/backend/core/types/globas.d.ts
+++ packages/backend/core/types/globas.d.ts
@@ -1,3 +1,5 @@
+type Primitives = string | number | boolean;
+
 type Lang = 'ru' | 'en';
 
 type HttpStatus = 200 | 301 | 302 | 400 | 403 | 404 | 410 | 500;
@@ -5,7 +7,10 @@ type HttpStatus = 200 | 301 | 302 | 400 | 403 | 404 | 410 | 500;
 type ShutdownCode = 'SIGINT' | 'SIGTERM';
 
 type JsonObject = {
-    [key: string]: string | number | boolean | JsonObject | JsonObject[]
+    [key: string]: Primitives | JsonObject | JsonObject[]
 }
+
+// Currently one-level deep
+type PlaceholderValue = Primitives | { [key: string]: Primitives };
 
 type Placeholders = JsonObject;
